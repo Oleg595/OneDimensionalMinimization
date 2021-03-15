eacd000dd5c2e934d3cebb57dec8e00e0c88541c
Kuksenko <60854701+kirillkuks@users.noreply.github.com>
2021-03-15 22:18:53 +0300

    add parabolic method

--- OneDimensionalMinimization/ParabolicMethod.cpp
+++ OneDimensionalMinimization/ParabolicMethod.cpp
@@ -14,5 +14,48 @@ void ParabolicMethod::set_section(std::pair<double, double> const& section_) {
 }
 
 double ParabolicMethod::calculate() {
-	return 0;
+	double x1 = section.first;
+	double x3 = section.second;
+
+	if (x3 - x1 >= tol) {
+		double f1 = function->at(x1);
+		double f2 = function->at(x3);
+		double x2 = (x1 + x3) / 2;
+		double f3 = function->at(x2);
+
+		while (x3 - x1 >= tol) {
+			double a1 = (f2 - f1) / (x2 - x1);
+			double a2 = 1.f / (x3 - x2) * ((f3 - f1) * (f2 - f1)) / ((x3 - x1) * (x2 - x1));
+
+			double x = 1.f / 2 * (x1 + x2 - a1 / a2);
+			double f = function->at(x);
+
+			if (x2 < x) {
+				if (f2 <= f) {
+					x3 = x;
+					f3 = f;
+				}
+				else {
+					x1 = x2;
+					f1 = f2;
+					x2 = x;
+					f2 = f;
+				}
+			}
+			else {
+				if (f <= f2) {
+					x3 = x2;
+					f3 = f2;
+					x2 = x;
+					f2 = f;
+				}
+				else {
+					x1 = x;
+					f1 = f;
+				}
+			}
+		}
+	}
+
+	return (x1 + x3) / 2;
 }
--- OneDimensionalMinimization/main.cpp
+++ OneDimensionalMinimization/main.cpp
@@ -6,6 +6,9 @@ int main() {
 	IFunction* function = IFunction::create_function([](double x) { return x * std::sin(x) + 2 * cos(x); });
 	IMethod* parabolic_method = IMethod::create_method(IMethod::METHOD::PARABOLIC, function, 0.02, { -5, -4 });
 
+	double  x_min = parabolic_method->calculate();
+	std::cout << "Number of function calls: " << function->get_counter() << std::endl;
+	std::cout << "Min: " << x_min << ", with value: " << function->at(x_min) << std::endl;
 
 	delete parabolic_method;
 	delete function;
